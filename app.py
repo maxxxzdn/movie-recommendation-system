b6b833d5632f0f7066a3157c16287aa422b1a634
maxxxzdn <50237680+maxxxzdn@users.noreply.github.com>
2020-06-06 07:38:25 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,9 @@
 from flask import Flask, render_template, request
 import numpy as np
-import pandas as pd
-from scipy import spatial
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy import spatial
-from search_functions import get_recommendations
 
-"""
-:metadata: pandas Series with metadata for each movie including keywords, genres, country of production, directors and actors
-:titles: pandas Seriies with title for each movie
-"""
-metadata = pd.read_csv('metadata.csv', squeeze=True)
-titles = pd.read_csv('titles.csv', squeeze=True)
-cosine_sim_path = 'cosine_sim2.txt'
+d = np.load('movie_dict.npy',allow_pickle='TRUE').item()
 
-# Count number of word occurences in metadata string for every movie
-#count = CountVectorizer(stop_words='english')
-#count_matrix = count.fit_transform(metadata)
-# Calculate cosine similarity to know how close metadata-vectors in word space to each other
-#cosine_sim = cosine_similarity(count_matrix, count_matrix)
-
-# Create an instance of the Flask class for our web app
 app = Flask(__name__)
-
 
 @app.route('/')
 def main():
@@ -35,10 +15,10 @@ def send(sum=sum):
     if request.method == 'POST':
         movie = request.form['movie']
         # Check if movie is presented in the database
-        if movie in titles.values:
-            result = get_recommendations(movie, titles, cosine_sim_path)
+        if movie in d.keys():
+            result = d[movie]
         else:
-            result = 'We dont have this movie in database'
+            result = []
 
         return render_template('app.html', result=result)
 
